a5e350a6abee4a502b68360825ac3f8c2609416b
Frank Baethge <baethge@kabelmail.de>
2015-04-26 17:44:50 +0200

    Settings repariert

--- phone/js/view/settings.js
+++ phone/js/view/settings.js
@@ -1,3 +1,28 @@
+// View für die Einstellungen 
+// ==========================
+
+define(function( require ) {
+	var $ = require('jquery');
+	var _ = require('underscore');
+	var Backbone = require('backbone');
+	var Fb3Model = require('model');
+	var settingsViewTemplate = require('text!../../templates/settingsView.html');
+
+	var SettingsView = Backbone.View.extend( {
+		el: '#settings',
+		model: fb3,
+
+		initialize: function() {
+			this.template = _.template(settingsViewTemplate);
+		},
+
+		events: {
+			// Hängt von den Elementen im Template ab
+			'click #save0DataButton': 'saveAll',
+			'change #dev': 'cDevice',
+			'change #vpn': 'cPerson',
+		},
+
 		render: function() {
 			fb3.countDS();
 			var compiledTemplate = this.template(fb3.settings);
